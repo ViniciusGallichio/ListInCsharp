486598375fa0eb79cff5aa2f74b425f002429da8
Vinicius M Gallichio <viniciusgallichio@gmail.com>
2021-04-27 15:32:08 -0300

    List1

---
+++ 3 List treinamento/Dados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace _3_List_treinamento
+{
+    class Dados
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public double Salary { get; private set; }
+
+        public Dados(string name, int id, double salary)
+        {
+            Name = name;
+            Id = id;
+            Salary = salary;
+        }
+
+        public void acres(double porcentage)
+        {
+            Salary += Salary * porcentage / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Id + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ 3 List treinamento/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace _3_List_treinamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos empregados você quer registrar? ");
+            Console.WriteLine();
+            int n = int.Parse(Console.ReadLine());
+
+            List<Dados> list = new List<Dados>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("EMPREGADO#" + i + ": ");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Salario: ");
+                double salary = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                list.Add(new Dados(name, id, salary));
+                Console.WriteLine();
+            }
+
+            Console.Write("Entre com o Id do funcionario que você aumentar o salario: ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Dados empreg = list.Find(x => x.Id == searchId);
+            if (empreg != null)
+            {
+                Console.Write("Coloque a porcentagem: ");
+                double porcentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                empreg.acres(porcentage);
+            }
+            else
+            {
+                Console.WriteLine("Esse empregado não existe: ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Lista atualizada");
+            foreach (Dados obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            
+        }
+    }
+}
